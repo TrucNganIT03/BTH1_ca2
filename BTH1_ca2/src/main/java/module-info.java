96c8a4ad89229d0600be92be60a084f433893b7f
TrucNganIT03 <2051052088ngan@ou.edu.vn>
2023-02-16 16:40:05 +0700

    Cap nhat them

--- BTH1_ca2/src/main/java/module-info.java
+++ BTH1_ca2/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.mycompany.bth1_ca2 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
     opens com.mycompany.bth1_ca2 to javafx.fxml;
     exports com.mycompany.bth1_ca2;
